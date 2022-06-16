3847974ca75bc2d03bfefc8c44744fef59da9d86
[diegohartmann] <[contato.hartmann@gmail.com]>
2022-06-16 18:35:09 -0300

    logo

---
+++ components/PageFooter/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from 'styles.module.css'
+const PageFooter = ({
+    href=`https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app`
+}) => {
+  return (
+    <footer className={styles.footer}>
+        <a
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            Powered by{' '}
+            <span className={styles.logo}>
+                <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            </span>
+        </a>
+    </footer>
+  )
+}
+
+export default PageFooter
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import DocHead from '../components/DocHead'
-// dev
+import PageFooter from '../components/PageFooter'
+
 const Home: NextPage = () => {
   return (
+    
     <div className={styles.container}>
+      
       <DocHead/>
 
       <main className={styles.main}>
@@ -49,7 +51,8 @@ const Home: NextPage = () => {
         </div>
       </main>
 
-      <footer className={styles.footer}>
+      <PageFooter/>
+      {/* <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -60,7 +63,7 @@ const Home: NextPage = () => {
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
-      </footer>
+      </footer> */}
       
     </div>
   )
